4ff96b4ec5c506e468aeea753d63392282fe7a97
Alexander Sack <asac@ubuntu.com>
2013-08-19 16:57:22 +0200

    since we now use top instead of vmstat, let's use s/vmstat/top/ for all variables as well

--- systemsettle/systemsettle.sh
+++ systemsettle/systemsettle.sh
@@ -19,7 +19,7 @@ cleanup () {
     # dont rerun this logic in case we get multiple signals
     dump_error=0
   fi
-  rm -f $top_log $vmstat_log $vmstat_log.reduced
+  rm -f $top_log $top_log.reduced
 }
 
 function show_usage() {
@@ -28,10 +28,10 @@ function show_usage() {
    echo "Options:"
    echo " -r  run forever without exiting"
    echo " -p  minimum idle percent to wait for (Default: 99)"
-   echo " -c  number of times to run vmstat at each iteration (Default: 10)"
-   echo " -d  seconds to delay between each vmstat iteration (Default: 6)"
-   echo " -i  vmstat measurements to ignore from each loop (Default: 1)"
-   echo " -m  maximum loops of vmstat before giving up if minimum idle"
+   echo " -c  number of times to run top at each iteration (Default: 10)"
+   echo " -d  seconds to delay between each top iteration (Default: 6)"
+   echo " -i  top measurements to ignore from each loop (Default: 1)"
+   echo " -m  maximum loops of top before giving up if minimum idle"
    echo "     percent is not reached (Default: 1)"
    exit 129
 }
@@ -44,11 +44,11 @@ while getopts "h?rp:c:d:i:m:" opt; do
               ;;
         p)    idle_avg_min=$OPTARG
               ;;
-        c)    vmstat_repeat=$OPTARG
+        c)    top_repeat=$OPTARG
               ;;
-        d)    vmstat_wait=$OPTARG
+        d)    top_wait=$OPTARG
               ;;
-        i)    vmstat_ignore=$OPTARG
+        i)    top_ignore=$OPTARG
               ;;
         m)    settle_max=$OPTARG
               ;;
@@ -57,43 +57,42 @@ done
 
 # minimum average idle level required to succeed
 idle_avg_min=${idle_avg_min:-99}
-# measurement details: vmstat $vmstat_wait $vmstat_repeat
-vmstat_repeat=${vmstat_repeat:-10}
-vmstat_wait=${vmstat_wait:-6}
+# measurement details: top $top_wait $top_repeat
+top_repeat=${top_repeat:-10}
+top_wait=${top_wait:-6}
 # how many samples to ignore
-vmstat_ignore=${vmstat_ignore:-1}
+top_ignore=${top_ignore:-1}
 # how many total attempts to settle the system
 settle_max=${settle_max:-10}
 
 # set and calc more runtime values
-vmstat_tail=`calc $vmstat_repeat - $vmstat_ignore`
+top_tail=`calc $top_repeat - $top_ignore`
 settle_count=0
 idle_avg=0
 
 echo "System Settle run - quiesce the system"
 echo "--------------------------------------"
 echo
-echo "  cmd            = 'top -b -d $vmstat_wait -n $vmstat_repeat' ignoring first $vmstat_ignore (tail: $vmstat_tail)"
+echo "  cmd            = 'top -b -d $top_wait -n $top_repeat' ignoring first $top_ignore (tail: $top_tail)"
 echo "  idle_avg_min   = '$idle_avg_min'"
-echo "  vmstat_repeat  = '$vmstat_repeat'"
-echo "  vmstat_wait    = '$vmstat_wait'"
-echo "  vmstat_ignore  = '$vmstat_ignore'"
+echo "  top_repeat  = '$top_repeat'"
+echo "  top_wait    = '$top_wait'"
+echo "  top_ignore  = '$top_ignore'"
 echo "  settle_max     = '$settle_max'"
 echo "  run_forever    = '$settle_prefix' (- = yes)"
 echo
 
 trap cleanup EXIT INT QUIT ILL KILL SEGV TERM
-vmstat_log=`mktemp -t`
 top_log=`mktemp -t`
 
 while test `calc $idle_avg '<' $idle_avg_min` = 1 -a "$settle_prefix$settle_count" -lt "$settle_max"; do
   echo -n "Starting system idle measurement (run: $settle_count) ... "
 
-  # get vmstat
+  # get top
   echo "TOP DUMP (after settle run: $settle_count)" >> $top_log
   echo "========================" >> $top_log
-  top -b -d $vmstat_wait -n $vmstat_repeat >> $top_log
-  cat $top_log | grep '.Cpu.*' | tail -n $vmstat_tail > $vmstat_log.reduced
+  top -b -d $top_wait -n $top_repeat >> $top_log
+  cat $top_log | grep '.Cpu.*' | tail -n $top_tail > $top_log.reduced
   echo >> $top_log
 
   # calc average of idle field for this measurement
@@ -103,7 +102,7 @@ while test `calc $idle_avg '<' $idle_avg_min` = 1 -a "$settle_prefix$settle_coun
      idle=`echo $line | sed -e 's/.* \([0-9\.]*\) id.*/\1/'`
      sum=`calc $sum + $idle`
      count=`calc $count + 1`
-  done < $vmstat_log.reduced
+  done < $top_log.reduced
 
   idle_avg=`calc $sum / $count`
   settle_count=`calc $settle_count + 1`
