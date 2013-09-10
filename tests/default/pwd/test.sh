38a9d0a92baf6d4570fc85ffb9fa8817e953332f
Andy Doan <andy.doan@canonical.com>
2013-09-10 08:51:04 -0400

    update "default" test to run from host or target
    This updates the default test cases to make use of the
    TARGET_PREFIX environment variable so that they can run
    from the target or host system

--- tests/default/pwd/test.sh
+++ tests/default/pwd/test.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
-echo pwd is: $(pwd)
-echo Changing to /tmp...
-cd /tmp
-echo Checking that cd worked...
-echo directory is now $(pwd), should be /tmp
-/usr/bin/test $(pwd) == '/tmp'
+#!/bin/sh
+
+set -e
+
+if [ -n "$TARGET_PREFIX" ] ; then
+	echo "RUNNING FROM HOST"
+else
+	echo "RUNNING ON TARGET"
+	TARGET_PREFIX="/bin/sh -c"
+fi
+
+#when it comes from adb we get a \r\n that should be removed
+dir=$($TARGET_PREFIX "cd /tmp; pwd" | head -n1 | tr -d '\r\n')
+if [ $dir != "/tmp" ] ; then
+	echo "failed to change directory"
+	exit 1
+fi
+exit 0
