0186fb9547aae8ab2487e1e852b2803dba183231
Paul Larson <paul.larson@canonical.com>
2013-09-24 11:32:44 -0500

    Check for updated files in devel-proposed
    The channel name is changing from daily-proposed to devel-proposed. The change needed for this in utah has already been made, we just need to look for updated images in the right location now.

--- jenkins/setup_jenkins.py
+++ jenkins/setup_jenkins.py
@@ -191,7 +191,7 @@ def _configure_master(instance, env, args, device, projects):
     trigger_url = ('http://cdimage.ubuntu.com/ubuntu-touch/daily-preinstalled/'
                    'pending/MD5SUMS')
     if SYSTEM_IMAGE:
-        fmt = 'http://system-image.ubuntu.com/daily-proposed/{}/index.json'
+        fmt = 'http://system-image.ubuntu.com/devel-proposed/{}/index.json'
         trigger_url = fmt.format(device_type)
 
     params = {
