8614c3a1f7f8e2d1441d12d6f1463ec8e3e5fa1d
Paul Larson <paul.larson@canonical.com>
2014-06-20 11:45:46 -0500

    well that was dumb...

--- tests/health-check/health-check-test-get-pids.py
+++ tests/health-check/health-check-test-get-pids.py
@@ -23,16 +23,16 @@ with open(os.path.join(basedir, 'procmapping.txt'), 'w') as mapping:
         try:
             proc = p.as_dict(attrs=['pid', 'name'])
             if os.getpgid(proc['pid']) != 0:
-                procname = os.path.basename(p['name'])
+                procname = os.path.basename(proc['name'])
                 fname = os.path.join(
                     default_threshold_path, procname + ".threshold")
                 count = procnames.get(procname, 0)
                 procnames[procname] = count + 1
                 if count > 0:
                     procname = '%s_%d' % (procname, count)
-                if not p['name'] in ignore_procs:
+                if not proc['name'] in ignore_procs:
                     if os.path.isfile(fname):
-                        mapping.write('%s:%d\n' % (procname, p['pid']))
+                        mapping.write('%s:%d\n' % (procname, proc['pid']))
                         print(procname)
         except:
             pass
