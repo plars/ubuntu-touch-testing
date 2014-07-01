231e4007df086e2ba41379aae105d8afb65cc75d
Parameswaram Sivatharman <para.siva@canonical.com>
2014-07-01 21:05:14 +0100

    Dont delete sylog and kern.log

--- scripts/run-autopilot-tests.sh
+++ scripts/run-autopilot-tests.sh
@@ -106,8 +106,7 @@ test_app() {
 reboot_wait() {
 	if [ -z $QUICK ] ; then
 		reboot-and-unlock.sh
-		FILES="/var/crash/* /home/phablet/.cache/upstart/*.log* \
-                       /var/log/syslog /var/log/kern.log"
+		FILES="/var/crash/* /home/phablet/.cache/upstart/*.log* "
 		if ! adb shell "rm -rf $FILES" ; then
 			log_error "unable to remove crash and log files, retrying"
 			adb wait-for-device
