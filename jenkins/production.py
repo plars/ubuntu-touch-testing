7d2b5c3c772d4813457a59d874e56a7f8f2e55fb
Paul Larson <paul.larson@canonical.com>
2014-07-25 17:42:09 -0500

    Add stable-staging-proposed channel as touch_stable

--- jenkins/production.py
+++ jenkins/production.py
@@ -20,22 +20,52 @@ UTOPIC_MATRIX = [
             {
                 'name': 'mako',
                 'slave-label': 'daily-mako',
-                'trigger_url': _url('utopic-proposed', 'mako'),
+                'trigger_url': _url('devel-proposed', 'mako'),
                 'num-workers': 3,
             },
             {
                 'name': 'flo',
                 'slave-label': 'daily-flo',
-                'trigger_url': _url('utopic-proposed', 'flo'),
+                'trigger_url': _url('devel-proposed', 'flo'),
                 'num-workers': 2,
             },
             {
                 'name': 'manta',
                 'slave-label': 'daily-manta',
-                'trigger_url': _url('utopic-proposed', 'manta'),
+                'trigger_url': _url('devel-proposed', 'manta'),
                 'num-workers': 2,
             },
         ],
+    },
+    {
+        'image-type': 'touch_stable',
+        'statsd-key': 'ubuntu-ci.daily-image.staging',
+        'include-qa': True,
+        'dashboard-host': 'ci.ubuntu.com',
+        'dashboard-port': '80',
+        'dashboard-user': 'doanac',
+        'devices': [
+            {
+                'name': 'mako',
+                'slave-label': 'daily-mako',
+                'trigger_url': _url('stable-staging-proposed', 'mako'),
+                'num-workers': 3,
+            },
+            {
+                'name': 'flo',
+                'slave-label': 'daily-flo',
+                'trigger_url': _url('stable-staging-proposed', 'flo'),
+                'num-workers': 2,
+            },
+            {
+                'name': 'manta',
+                'slave-label': 'daily-manta',
+                'trigger_url': _url('stable-staging-proposed', 'manta'),
+                'num-workers': 2,
+            },
+        ],
+        'IMAGE_OPT': 'export IMAGE_OPT="--bootstrap --developer-mode '
+                     '--channel ubuntu-touch/stable-staging-proposed"'        
     },
     {
         'image-type': 'touch_custom',
