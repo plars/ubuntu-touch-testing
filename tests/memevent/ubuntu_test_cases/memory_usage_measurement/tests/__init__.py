a3f972c4cc1db4973859be198ca1a5b6665d65a6
Christopher Lee <chris.lee@canonical.com>
2014-08-05 17:55:40 +1200

    Using a mixin overriding class to handle smem probe.

--- tests/memevent/ubuntu_test_cases/memory_usage_measurement/tests/__init__.py
+++ tests/memevent/ubuntu_test_cases/memory_usage_measurement/tests/__init__.py
@@ -3,44 +3,64 @@
 import json
 import logging
 
-from autopilot.testcase import AutopilotTestCase
-from autopilot.input import Mouse, Pointer, Touch
-from autopilot.platform import model
+from testtools import TestCase
 
 from ubuntu_test_cases.memory_usage_measurement.probes import SmemProbe
 
 LOGGER = logging.getLogger(__file__)
 
 
-class MemoryUsageTests(AutopilotTestCase):
+class MemoryUsageTestsMixin(object):
+    def launch_test_application(self, application, *arguments, **kwargs):
+        self.smem.start("Application Launch")
+        launched_app = super().launch_test_application(
+            application,
+            *arguments,
+            **kwargs
+        )
+        self.smem.follow(launched_app.pid)
+        self.smem.stop("Application Launch")
+        self.smem.start(self.eventname)
+
+        self.addCleanup(self.smem.stop, self.eventname)
+        return launched_app
+
+
+class MemoryUsageTests(TestCase):
 
     """Event based memory usage measurement scenario."""
 
-    @classmethod
-    def setUpClass(cls, appname):
-        cls._app_id_name = appname
-        cls.smem = SmemProbe()
+    def setUp(self, app_name):
+        super().setUp()
+        self.app_name = app_name
+        self.smem = SmemProbe()
 
-    @classmethod
-    def tearDownClass(cls):
-        """Write report to to results directory."""
+    def memtest_run_test(self, test_class, test_id, eventname):
+        print("Overriding the method, and running the test:")
 
-        report_filename = '/tmp/memory_usage_{}.json'.format(cls._app_id_name)
+        TestClass = type(
+            "MemTestTestClass",
+            (MemoryUsageTestsMixin, test_class),
+            {'smem': self.smem, 'eventname': eventname}
+        )
+
+        t = TestClass(test_id)
+        result = t.run()
+        if not result.wasSuccessful():
+            raise Exception()
+        self.addCleanup(self._write_report)
+
+    def _write_report(self):
+        test_id_name = self.id().split('.')[-1]
+        report_filename = '/tmp/memory_usage_{}.{}.json'.format(
+            self.app_name,
+            test_id_name
+        )
         with open(report_filename, 'w') as report_file:
             json.dump(
-                cls.smem.report,
+                self.smem.report,
                 report_file,
                 indent=4,
                 sort_keys=True
             )
             LOGGER.debug('Report written to {}'.format(report_file.name))
-
-    def setUp(self):
-        super().setUp()
-        """Scenario that takes measurements on some events."""
-
-        if model() == 'Desktop':
-            input_device = Mouse.create()
-        else:
-            input_device = Touch.create()
-        self.pointer = Pointer(input_device)
--- tests/memevent/ubuntu_test_cases/memory_usage_measurement/tests/test_browser.py
+++ tests/memevent/ubuntu_test_cases/memory_usage_measurement/tests/test_browser.py
@@ -1,4 +1,3 @@
-
 from testtools.matchers import Equals
 from webbrowser_app.tests import BrowserTestCaseBase
 
@@ -8,31 +7,22 @@ from ubuntu_test_cases.memory_usage_measurement.matchers import (
 )
 
 
-class BrowserMemoryTestCase(BrowserTestCaseBase, MemoryUsageTests):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass("webbrowser-app")
-
-    # Override this method so we can store the pid used.
-    def launch_test_application(self, application, *arguments, **kwargs):
-        launched_app = super().launch_test_application(
-            application,
-            *arguments,
-            **kwargs
-        )
-        self._browser_pid = launched_app.pid
-        return launched_app
-
-    def test_launch_webbrowser(self):
-        """Simple test, browser launched."""
-        with self.smem.probe('Browser started'):
-            self.assertThat(self.main_window.visible, Eventually(Equals(True)))
-            self.smem.follow(self._browser_pid)
-
+# Use the browsers autopilot test cases methods.
+class SingleTestCaseBase(BrowserTestCaseBase):
     def test_navigate_to_url(self):
-        with self.smem.probe('Browser finished loading'):
-            self.assertThat(self.main_window.visible, Eventually(Equals(True)))
-            url = 'http://acid3.acidtests.org/'
-            self.go_to_url(url)
-            self.assert_page_eventually_loaded(url)
-            self.smem.follow(self._browser_pid)
+        self.assertThat(self.main_window.visible, Eventually(Equals(True)))
+        url = 'http://acid3.acidtests.org/'
+        self.go_to_url(url)
+        self.assert_page_eventually_loaded(url)
+
+
+class BrowserMemoryTestCase(MemoryUsageTests):
+    def setUp(self):
+        super().setUp('webbrowser-app')
+
+    def test_webbrowser_navigation(self):
+        self.memtest_run_test(
+            SingleTestCaseBase,
+            "test_navigate_to_url",
+            "Navigate to url"
+        )
