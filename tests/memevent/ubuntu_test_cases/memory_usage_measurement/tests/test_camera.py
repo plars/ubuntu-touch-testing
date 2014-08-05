65d9596c21f6002f7824ee553c88ddcf4cc4b9fc
Christopher Lee <chris.lee@canonical.com>
2014-08-05 15:45:29 +1200

    Add camera tests using new method

--- tests/memevent/ubuntu_test_cases/memory_usage_measurement/tests/test_camera.py
+++ tests/memevent/ubuntu_test_cases/memory_usage_measurement/tests/test_camera.py
@@ -2,10 +2,8 @@ import logging
 import os
 from glob import glob
 
-from camera_app.emulators.main_window import MainWindow as CameraWindow
+from camera_app.tests import CameraAppTestCase
 from testtools.matchers import Equals, HasLength
-
-# from ubuntuuitoolkit.emulators import UbuntuUIToolkitEmulatorBase
 
 from ubuntu_test_cases.memory_usage_measurement.tests import MemoryUsageTests
 from ubuntu_test_cases.memory_usage_measurement.matchers import (
@@ -15,21 +13,54 @@ from ubuntu_test_cases.memory_usage_measurement.matchers import (
 LOGGER = logging.getLogger(__file__)
 
 
-class CameraMemoryUsageTests(MemoryUsageTests):
+class CameraMemoryTestCase(CameraAppTestCase, MemoryUsageTests):
+
     IMAGE_FILENAME_PATTERN = os.path.expanduser('~/Pictures/camera/image*')
 
-    def setUp(self):
-        super().setUp("camera-app")
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass("camera-app")
 
-    def test_camera_usage(self):
-        # camera = CameraApp(self)
+    # Override this method so we can store the pid used.
+    def launch_test_application(self, application, *arguments, **kwargs):
+        launched_app = super().launch_test_application(
+            application,
+            *arguments,
+            **kwargs
+        )
+        self._camera_pid = launched_app.pid
+        return launched_app
+
+    def test_launch_camera_app(self):
+        """Simple test, camera launched."""
         with self.smem.probe('Camera app started'):
-            self.launch()
-            # self.smem.pids.append(self.app.pid)
-            self.smem.follow(self.app.pid, "camera-app")
+            self.assertThat(
+                self.main_window.get_qml_view().visible,
+                Eventually(Equals(True))
+            )
+            self.smem.follow(self._camera_pid)
+
+    def test_take_picture(self):
+        self._remove_image_files()
 
         with self.smem.probe('Camera app picture taken'):
-            self.take_picture()
+            self.press_exposure_button()
+
+            # Wait for image file to be produced before memory usage
+            # measurement
+            self.assertThat(
+                lambda: glob(self.IMAGE_FILENAME_PATTERN),
+                Eventually(HasLength(1))
+            )
+            self.smem.follow(self._camera_pid)
+
+    def press_exposure_button(self):
+        exposure_button = self.main_window.get_exposure_button()
+        self.assertThat(exposure_button.enabled, Eventually(Equals(True)))
+
+        LOGGER.debug('Taking picture...')
+        self.pointer.move_to_object(exposure_button)
+        self.pointer.click()
 
     def _remove_image_files(self):
         """Remove all image files.
@@ -41,31 +72,3 @@ class CameraMemoryUsageTests(MemoryUsageTests):
         for filename in glob(self.IMAGE_FILENAME_PATTERN):
             LOGGER.debug('Removing image file: {}'.format(filename))
             os.remove(filename)
-
-    def launch(self):
-        """Launch application."""
-        args = [
-            'camera-app',
-            '--fullscreen',
-            ('--desktop_file_hint='
-             '/usr/share/applications/camera-app.desktop'),
-        ]
-        self.app = self.launch_test_application(*args, app_type='qt')
-        self.window = CameraWindow(self.app)
-
-    def take_picture(self):
-        """Click on the exposure button to take picture."""
-        self._remove_image_files()
-
-        exposure_button = self.window.get_exposure_button()
-        self.assertThat(exposure_button.enabled, Eventually(Equals(True)))
-
-        LOGGER.debug('Taking picture...')
-        self.pointer.move_to_object(exposure_button)
-        self.pointer.click()
-
-        # Wait for image file to be produced before memory usage measurement
-        self.assertThat(
-            lambda: glob(self.IMAGE_FILENAME_PATTERN),
-            Eventually(HasLength(1))
-        )
