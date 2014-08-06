0ec5edcb0e8bea954a2d05291e63789a9d21072e
Christopher Lee <chris.lee@canonical.com>
2014-08-06 15:33:54 +1200

    Update nfss upload script to handle the potential multiple files per application.

--- tests/memevent/nfss_upload_results.py
+++ tests/memevent/nfss_upload_results.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 
-"""This script takes a memevent-test result json and uploads any interesting
-data contained within.
+"""This script takes memevent-test result json filse and uploads the
+interesting data contained within to a nfss backend.
 
 It will process any memory result files found in /tmp/ matching the naming
 scheme: "/tmp/memory_usage_*.json"
 
 """
 
-import json
-import sys
 import datetime
-import subprocess
-from glob import glob
+import json
 import re
-from requests_oauthlib import OAuth1Session
+import sys
+import subprocess
+
+from collections import defaultdict
+from glob import glob
 
 json_results_filename_pattern = "/tmp/memory_usage_*.json"
 
@@ -27,33 +28,6 @@ def _get_run_details(app_name):
         package_version=_get_package_version(app_name),
         application_name=app_name,
     )
-
-
-def _get_app_details(app_name, json_data):
-    """Given the json data extract out the details for each application
-    contained within.
-
-    incl. package version
-
-    """
-
-    app_results = []
-    pids = json_data["pids"]
-    for reading in json_data["readings"]:
-        results = dict(
-            event=reading["event"],
-            start_time=reading["start_time"],
-            stop_time=reading["stop_time"],
-        )
-        # find the data results for this event (based on pid).
-        for pid in pids:
-            if str(pid) in reading["data"].keys():
-                results["data"] = reading["data"][str(pid)]
-                results["pid"] = pid
-                break
-        app_results.append(results)
-
-    return app_results
 
 
 def _get_package_version(appname):
@@ -90,7 +64,7 @@ def _upload_data(test_name, run_json):
         print("Error: Data does not appear to be valid json: %s" % e)
         sys.exit(3)
 
-    print("...")
+    print("...:memevent:-", test_name)
     print(run_json_string)
     print("...")
     # try:
@@ -101,23 +75,73 @@ def _upload_data(test_name, run_json):
     #     print("Unable to upload data: ", e)
 
 
-def _get_app_name_from_filename(filename):
-    "memory_usage_*.json"
-    app_name_search = re.search('memory_usage_(.*)\.json', filename)
-    return app_name_search.group(1)
+def _get_files_app_name_and_test(filename):
+    """return tuple containing (appname, testname)."""
+    app_name_search = re.search(
+        'memory_usage_([a-zA-Z-]*).(.*)\.json',
+        filename
+    )
+    return (app_name_search.group(1), app_name_search.group(2))
+
+
+def get_application_readings(json_data):
+    app_results = []
+    pids = json_data["pids"]
+    for reading in json_data["readings"]:
+        results = dict(
+            event=reading["event"],
+            start_time=reading["start_time"],
+            stop_time=reading["stop_time"],
+        )
+        # find the data results for this event (based on pid).
+        for pid in pids:
+            if str(pid) in reading["data"].keys():
+                results["data"] = reading["data"][str(pid)]
+                results["pid"] = pid
+                break
+        app_results.append(results)
+    return app_results
+
+
+def get_application_results(json_filepath):
+    with open(json_filepath, "r") as f:
+        json_data = json.load(f)
+    return get_application_readings(json_data)
+
+
+def get_application_details(application_files):
+    """For every file this application has grab out the details and return a
+    list dictionaries containing reading details.
+
+    """
+    app_result = []
+    for json_file in application_files:
+        app_result.extend(get_application_results(json_file))
+    return app_result
+
+
+def map_files_to_applications():
+    """For any memory result files that exist, return a dictionary whos keys
+    are the applications name mapped to the file.
+
+    We can then produce a single json result for each application regardless of
+    there being many tests / results for it.
+
+    """
+
+    file_map = defaultdict(list)
+    for json_result_file in glob(json_results_filename_pattern):
+        app_name, test_name = _get_files_app_name_and_test(json_result_file)
+        file_map[app_name].append(json_result_file)
+    return file_map
+
 
 if __name__ == '__main__':
-    # target file is passed in
-    # Open file and load json data
-    # Create an entry for each application-name contained within the file
-    # after all entries read, for each entry created upload the details to nfss
-    # backend.
+    app_details = dict()
+    file_map = map_files_to_applications()
+    for app_name in file_map.keys():
+        app_details[app_name] = get_application_details(file_map[app_name])
 
-    for json_result_file in glob(json_results_filename_pattern):
-        with open(json_result_file, "r") as f:
-            json_data = json.load(f)
-        app_name = _get_app_name_from_filename(json_result_file)
+    for app_name in app_details.keys():
         run_details = _get_run_details(app_name)
-        application_details = _get_app_details(app_name, json_data)
-
-        upload_json_details(run_details, application_details)
+        upload_json_details(run_details, app_details[app_name])
