81d26c160436614b40b33e5bdf4131a3a938759f
Paul Larson <paul.larson@canonical.com>
2014-08-06 15:34:29 -0500

    wait for the device to come back after running suspend blocker

--- tests/suspend-blocker/suspend-blocker/suspend-blocker.sh
+++ tests/suspend-blocker/suspend-blocker/suspend-blocker.sh
@@ -17,6 +17,13 @@ ncd_usb.py -u http://qa-relay-control -b 0 -r 0 off
 sleep 1800
 #Turn on the USB port for this device
 ncd_usb.py -u http://qa-relay-control -b 0 -r 0 on
+# Wait for the device to come back
+sleep 10
+if ! ${TARGET_PREFIX} sudo -iu phablet stop susblock; then
+	echo "Device not available yet, waiting a bit longer"
+	sleep 30
+	${TARGET_PREFIX} sudo -iu phablet stop susblock
+fi
 ${TARGET_PREFIX} sudo -iu phablet stop susblock
 ${TARGET_PREFIX} /tmp/suspend-blocker/suspend-blocker -rbH -o /tmp/results/kern.json /var/log/kern.log
 ${TARGET_PREFIX} cp /var/log/kern.log /tmp/results
