83a77773f7f583666ad34905ed61e94ef112ef3b
Paul Larson <paul.larson@canonical.com>
2014-08-26 12:05:55 -0500

    capture /var/log/whoopsie.log for all test runs

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -57,6 +57,7 @@ test_from_host() {
 		--pull /tmp/xmlresults \
                 --pull /var/log/syslog \
                 --pull /var/log/kern.log \
+                --pull /var/log/whoopsie.log \
 		$EXTRA_PULL \
 		-l ${TESTSUITE_HOST}/master.run
 
