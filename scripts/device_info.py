50c2cf80a5a0b22bc257d9e752a8bbbbdfe101a4
Paul Larson <paul.larson@canonical.com>
2014-11-05 14:34:00 -0600

    just use stable image for all devices

--- scripts/device_info.py
+++ scripts/device_info.py
@@ -58,19 +58,11 @@ class TouchDevice(object):
     def reimage_from_fastboot(self):
         #Starting from fastboot mode, put a known-good image on the device
         log.info("Flashing the last stable image")
-        if self.devtype == "krillin":
-            subprocess.check_output(['ubuntu-device-flash', '--serial',
-                                     self.serial, '--channel',
-                                     'ubuntu-touch/ubuntu-rtm/14.09-proposed',
-                                     '--bootstrap', '--revision=141',
-                                     '--developer-mode',
-                                     '--password', 'ubuntuci'])
-        else:
-            subprocess.check_output(['ubuntu-device-flash', '--serial',
-                                     self.serial, '--channel',
-                                     'ubuntu-touch/stable', '--bootstrap',
-                                     '--developer-mode',
-                                     '--password', 'ubuntuci'])
+        subprocess.check_output(['ubuntu-device-flash', '--serial',
+                                 self.serial, '--channel',
+                                 'ubuntu-touch/stable', '--bootstrap',
+                                 '--developer-mode',
+                                 '--password', 'ubuntuci'])
         return self.wait_for_device(600)
 
     def wait_for_fastboot(self, timeout=120):
