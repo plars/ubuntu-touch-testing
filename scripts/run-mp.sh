fbc46c44c6728456f21b18e492693c5f7f681297
Francis Ginther <francis.ginther@canonical.com>
2014-11-06 13:21:58 -0600

    Add scripts/run-mp.sh to facilitate testing MPs via a jenkins job, and making it easier for developers to reproduce the same results.

---
+++ scripts/run-mp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+# These are all set via jenkins when running in that context
+if [ -z "${ANDROID_SERIAL}" ] || [ -z "${package_archive}" ] || \
+	[ -z "${test_packages}" ] || [ -z "${test_suite}" ]; then
+	echo "Missing an env variable: "
+	echo "    ANDROID_SERIAL, package_archive, test_packages or test_suite"
+	exit 1
+fi
+
+BASEDIR=$(dirname $(readlink -f $0))/..
+ARCHIVE_TMP=$(mktemp -d)
+trap 'rm -rf "${ARCHIVE_TMP}"' EXIT HUP INT TERM
+
+wget -O "${ARCHIVE_TMP}/archive.zip" ${package_archive}
+unzip "${ARCHIVE_TMP}/archive.zip" -d "${ARCHIVE_TMP}"
+package_dir="${ARCHIVE_TMP}/archive"
+
+# This is the list of packages to be installed from the archive
+# It's manually generated and supplied via the lp:cupstream2distro-config
+# configuration files when executed by jenkins.
+package_list=""
+for package in ${test_packages}; do
+	package_list="-p ${package} ${package_list}"
+done
+
+# The provision.sh and run-smoke scripts can install extra packages to meet
+# the needs of image testing. Since we are installing specific packages from
+# a local archive, this nneds to be disabled.
+export SKIP_CLICK=1
+export SKIP_TESTCONFIG=1
+
+# Provision the device and run the test suite.
+${BASEDIR}/scripts/provision.sh -s ${ANDROID_SERIAL} \
+	-n ${HOME}/.ubuntu-ci/wifi.conf \
+	-D ${package_dir} ${package_list}
+${BASEDIR}/scripts/run-smoke -s ${ANDROID_SERIAL} -n -a ${test_suite}
