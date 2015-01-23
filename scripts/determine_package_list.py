9a6ffbfe52d1f98cd843f3bb9bb13e02edcc3d28
Francis Ginther <francis.ginther@canonical.com>
2015-01-22 21:29:31 -0600

    Add some tests for determine_package_list.py and some refactoring.

--- scripts/determine_package_list.py
+++ scripts/determine_package_list.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python
 
+# Ubuntu Test Cases for Touch
+# Copyright 2015 Canonical Ltd.
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
 import subprocess
 import sys
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+ADB_SHELL = [os.path.join(HERE, '../utils/host/adb-shell')]
+
+
+def _adb_shell(command):
+    '''Wrapper for adb commands'''
+    cmd_args = []
+    cmd_args.extend(ADB_SHELL)
+    cmd_args.extend(command)
+    return subprocess.check_output(cmd_args)
+
 
 def get_input(in_file_name):
     '''Gets the source package input from the json encoded file'''
@@ -12,20 +40,22 @@ def get_input(in_file_name):
 
 
 def get_binary_package_from_source(source_package):
-    '''Return a list of binary packages from the given source package.'''
-    subprocess.check_output(['adb', 'shell', 'apt-get update'])
-    showsrc = subprocess.check_output(
-        ['adb', 'shell', 'apt-cache showsrc {}'.format(source_package)])
+    '''Return a list of binary packages from the given source package
+
+    Prior to executing this, 'apt-get update' must have been been called on
+    the target adb device to acquire the source package data.
+    '''
+    showsrc = _adb_shell(['apt-cache showsrc {}'.format(source_package)])
     for line in showsrc.splitlines():
         if line.startswith('Binary:'):
             return line[len('Binary: '):].split(', ')
+    return []
 
 
 def get_all_package_list():
     '''Return a list of all packages on the touch device'''
     package_list = []
-    packages = subprocess.check_output(
-        ['adb', 'shell', 'dpkg --get-selections'])
+    packages = _adb_shell(['dpkg --get-selections'])
     for line in packages.splitlines():
         package_name = line.split()[0]
         # XXX - fginther 20150120
@@ -44,4 +74,4 @@ if __name__ == '__main__':
     binary_packages = get_binary_package_from_source(source_package)
     intersection = set(all_packages) & set(binary_packages)
     print(' '.join(intersection))
-    sys.exit(0) 
+    sys.exit(0)
