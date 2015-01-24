ce872483d74f1c98357650764a61202910478bff
Vincent Ladeuil <vila+ci@canonical.com>
2015-01-24 16:21:02 +0100

    Plan B: - get the source package tree from inside the testbed, - inject the boottest there (overriding debian/tests/control) - call adt-run there
    This delegates more work to adt-run so only the package source name is
    required.

--- scripts/boottest.sh
+++ scripts/boottest.sh
@@ -20,7 +20,6 @@ export VERSION=${VERSION:-1.2.51-0ubuntu3}
 
 
 PHABLET_PASSWORD="${PHABLET_PASSWORD-0000}"
-TEST_SOURCE=${BASEDIR}/tests/boottest
 
 # The provision.sh and run-smoke scripts can install extra packages to meet
 # the needs of image testing. Since we are installing specific packages from
@@ -31,6 +30,8 @@ export SKIP_TESTCONFIG=1
 # Ensures we start with a usable phone
 test-runner/scripts/recover.py ${NODE_NAME}
 
+TESTS=${BASEDIR}/tests
+
 # Provision the device
 # FIXME: workaround #82 being unbootable for krillin assuming we run on
 # dev-jenkins until this is fixed -- vila 2015-01-23
@@ -39,28 +40,38 @@ ${BASEDIR}/scripts/provision.sh -s ${ANDROID_SERIAL} \
     -r $REVISION \
 	-n ${HOME}/.ubuntu-ci/wifi.conf -w
 
-# Modify the debian changelog in boottest to show that it's testing the 
-# package and version we care about
-sed -e "s/{{ source_package }}/${SRC_PKG_NAME}/" \
-    -e "s/{{ package_version }}/${VERSION}/" \
-    -e "s/{{ series }}/${RELEASE}/" \
-    ${TEST_SOURCE}/debian/changelog.template > ${TEST_SOURCE}/debian/changelog
-
-# Lookup the binary packages installed for the given source package
-#packages=$($BASEDIR/scripts/boottest.py -b ${SRC_PKG_NAME})
-#echo $packages
-
 # Generate the adt-run setup-command
 rm -f adt-commands || true
+# FIXME: '-proposed' will be setup by adt-run but we still need one apt-get
+# update call (vila -> pitti: why ?) -- vila 2015-01-24
 echo "apt-get update" > adt-commands
 
-# Now execute the test
-# - from $TEST_SOURCE containing only the boottest dep8 test
+
+# --no-built-binaries should come first
+ADT_CMD="adt-run --no-built-binaries"
 # - setting up -proposed and doing apt-get update
 # - via adt-virt-ssh with a setup from adb
 # - pitti said to use '--apt-upgrade' but that fails on the phone
 # (http://dev-jenkins.ubuntu-ci:8080/job/vila-bootesting/10/console)
-adt-run --no-built-binaries --unbuilt-tree ${TEST_SOURCE} \
+ADT_OPTS= --apt-pocket=proposed \
+    --setup-commands=adt-commands \
+    --- adt-virt-ssh -s /usr/share/autopkgtest/ssh-setup/adb \
+    -- -s "${ANDROID_SERIAL}"
+
+# Execute a first test to get the package source tree from the testbed.
+PKG_SRC_DIR=${BASEDIR}/APT-GET-SOURCE
+rm -fr ${PKG_SRC_DIR} || true
+${ADT_CMD} --unbuilt-tree ${TESTS}/get-pkg-src -o ${PKG_SRC_DIR}
+
+# Inject the boot DEP8 test into the package source tree
+SOURCE_DIR=="${PKG_SRC_DIR}/${SRC_PKG_NAME}*"
+FROM=${TESTS}/boottest/debian/tests
+TARGET="${SOURCE_DIR}*/debian/tests"
+mkdir -p ${TARGET} # For packages that don't define DEP8 tests
+cp ${FROM}/control ${FROM}/boottest ${TARGET}
+
+# Now execute the boot test from inside the pkg source tree
+adt-run --no-built-binaries --unbuilt-tree ${SOURCE_DIR} \
      -o results \
     --apt-pocket=proposed \
     --setup-commands=adt-commands \
