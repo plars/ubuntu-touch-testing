0e584d708f4df88ef3cb25a4455adf887c81ae05
Paul Larson <paul.larson@canonical.com>
2015-04-28 12:51:19 -0500

    Add jenkins setup config for arale

---
+++ jenkins/production-arale.py
@@ -0,0 +1,29 @@
+# The configuration matrix of our staging device testing
+
+JENKINS = 'http://dev-jenkins.ubuntu-ci:8080/'
+
+VIVID_MATRIX = [
+    {
+        'image-type': 'touch',
+        'statsd-key': 'ubuntu-ci.daily-image.staging',
+        'include-qa': True,
+        'dashboard-host': 'dashboard.ubuntu-ci',
+        'dashboard-port': '8080',
+        'dashboard-user': 'ci-bot',
+        'devices': [
+            {
+                'name': 'arale',
+                'slave-label': 'arale',
+                'trigger_url': 'https://sis.capomastro.canonical.com/ubuntu-touch/tangxi-vivid-proposed/arale/index.json',
+                'num-workers': 4,
+            }
+        ],
+        'IMAGE_OPT': 'export IMAGE_OPT="--bootstrap --developer-mode '
+                     '--channel=ubuntu-touch/vivid-proposed --device=m75"',
+        'IMAGE_SERVER': 'export IMAGE_SERVER="--server https://sis.capomastro.canonical.com"'
+    },
+]
+
+MATRIX = {
+    'vivid': VIVID_MATRIX,
+}
